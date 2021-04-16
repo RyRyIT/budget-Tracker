f7173bf3378b8259ca327aded18ca0c76221f796
Ryan Lackey <lackeyr13@gmail.com>
2021-04-15 22:57:28 -0400

    Done?

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,54 +1,70 @@
-const cacheData = [
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "db-cache-v1";
+const FILES_TO_CACHE = [
   "/",
-  "/public/db.js",
-  "/public/index.html",
-  "/public/index.js",
-  "/public/service-worker.js",
-  "/public/styles.css",
+  "./db.js",
+  "./index.html",
+  "./index.js",
+  "./service-worker.js",
+  "./styles.css",
 ];
 
-const cacheName = "cache-version1";
-const dbcacheName = "dbcache-version1";
-
-self.addEventListener("installation", (event) => {
-  event.waitUntil(
-    caches
-      .open(cacheName)
-      .then((cache) => {
-        return cache.addAll(cacheData);
-      })
-      .then(self.skipWaiting())
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("files pre-cashe Complete!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (event) => {
-  if (event.request.url.includes("/api/")) {
-    event.respondWith(
-      caches.open(dbcacheName).then((cache) => {
-        return fetch(event.request)
-          .then((response) => {
-            return cache
-              .put(event.request, response.clone())
-              .then(() => {
-                return response;
-              })
-              .catch((err) => {
-                return cache.match(err);
-              });
-          })
-          .catch((err) => console.log(err));
-      })
+self.addEventListener("activate", function (evt) {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+self.addEventListener("fetch", function (evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+            
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+           
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
     );
+
     return;
   }
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request).then((response) => {
-        if (response) {
-          return response;
-        } else if (event.request.headers.get("accept").then("text/html")) {
-          return cache.match("/");
-        }
+
+  evt.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(evt.request).then((response) => {
+        return response || fetch(evt.request);
       });
     })
   );
